8159c0fe58506b8af002b7f1a81e75998e70f59a
Daniel Krawiec <daniel.krawiec@itechart-group.com>
2022-08-11 15:09:12 +0200

    added fetchUsers

---
+++ tests/fetchUsers.test.js
@@ -0,0 +1,32 @@
+import axios from 'axios'
+
+const { BASE_URL, fetchUsers } = require('../src/fetchUsers');
+
+jest.mock("axios");
+
+describe("fetchUsers", () => {
+	it("should return users list", async () => {
+		const users = [
+			{ id: 1, name: "name1" },
+			{ id: 2, name: "name2" },
+		];
+
+		axios.get.mockResolvedValueOnce(users);
+
+		const result = await fetchUsers();
+
+		expect(axios.get).toHaveBeenCalledWith(`${BASE_URL}/users`);
+		expect(result).toEqual(users);
+
+	})
+
+	it("should return empty users list", async () => {
+		const error = "no users";
+    axios.get.mockRejectedValueOnce(new Error(error));
+
+		const result = await fetchUsers();
+
+		expect(axios.get).toHaveBeenCalledWith(`${BASE_URL}/users`);
+		expect(result).toEqual([]);
+  });
+});
