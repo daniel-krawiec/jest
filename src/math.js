3de55adc06db977bc361b7f4cdaead99f46b22bf
Daniel Krawiec <daniel.krawiec@itechart-group.com>
2022-08-11 12:53:17 +0200

    added math and hello tests

---
+++ src/math.js
@@ -0,0 +1,6 @@
+const adding = (a, b) => a + b;
+const multiplying = (a, b) => a * b;
+const substracting = (a, b) => a - b;
+const dividing = (a, b) => a / b;
+
+module.exports = { adding, multiplying, substracting, dividing };
---
+++ tests/helloWorld.test.js
@@ -0,0 +1,22 @@
+const helloWorld = require('../src/helloWorld');
+
+it("answers 'Is it a Hello?' if you ask a question", () => {
+  expect(helloWorld("How are you?")).toBe("Is it a Hello?");
+});
+
+it("answers 'Finish Hello' if you won't finish", () => {
+  expect(helloWorld("Say...")).toBe("Finish Hello");
+});
+
+it("answers 'It's a Hello!' when there is exclamation", () => {
+  expect(helloWorld("Hello!")).toBe("It's a Hello!");
+});
+
+it("shows 'Empty' if there is nothing.' ", () => {
+  expect(helloWorld("")).toBe("Empty");
+});
+
+it("answers 'Hello World' to anything else", () => {
+  expect(helloWorld("asdasdasd")).toBe("Hello World");
+});
+
